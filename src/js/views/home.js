f203c9c21719da06a13644aa61fe71002491726b
kevincastro015 <kevin.castro015@mymdc.net>
2021-08-08 17:21:16 +0000

    missionProcess

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,11 +4,13 @@ import { useHistory } from "react-router-dom";
 
 export const Home = () => {
 	return (
-		<div id="homeBackground" className="bg-dark text-light">
-			<h2>
-				Our mission is
-				<span className="highlight-text"> simple,</span> to bring those who are far from God close to him.
-			</h2>
+		<div className="container">
+			<div id="homeBackground" className="bg-dark text-light">
+				<h3>
+					Our mission is
+					<span className="highlight-text"> simple,</span> to bring those who are far from God close to him.
+				</h3>
+			</div>
 		</div>
 	);
 };
