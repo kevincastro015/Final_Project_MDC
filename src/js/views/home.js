122e22fa5bee19acea6b690f212566a3f5bb81b2
kevincastro015 <kevin.castro015@mymdc.net>
2021-08-08 16:54:58 +0000

    homeStyle

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,5 +3,12 @@ import { singIn } from "../utilities/signIn";
 import { useHistory } from "react-router-dom";
 
 export const Home = () => {
-	return <div>1st page</div>;
+	return (
+		<div id="homeBackground" className="bg-dark text-light">
+			<h2>
+				Our mission is
+				<span className="highlight-text"> simple,</span> to bring those who are far from God close to him.
+			</h2>
+		</div>
+	);
 };
