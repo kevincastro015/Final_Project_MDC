04c06331e10badbb2f55509139a0c96d04ec1956
Marcelo C Alves <marcelocalves@live.com>
2021-08-09 23:53:56 +0000

    Login Design

--- src/js/views/LogIn.js
+++ src/js/views/LogIn.js
@@ -26,51 +26,88 @@ export const LogIn = () => {
 	};
 
 	return (
-		<span>
-			<div className="sidenav">
-				<div className="login-main-text">
-					<h2>
-						Application
-						<br /> Login Page
-					</h2>
-					<p>Login or register from here to access.</p>
+		<div className="container ">
+			<div className="row justify-content-center">
+				<div className="col-4 mt-3 p-3">
+					<form>
+						<div className="form-group">
+							<label htmlFor="exampleInputEmail1">Email address</label>
+							<input
+								type="email"
+								className="form-control"
+								id="exampleInputEmail1"
+								aria-describedby="emailHelp"
+								onChange={e => setEmail(e.target.value)}
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="exampleInputPassword1">Password</label>
+							<input
+								type="password"
+								className="form-control"
+								id="exampleInputPassword1"
+								onChange={e => setPassword(e.target.value)}
+							/>
+						</div>
+						<button
+							type="submit"
+							className="btn btn-primary"
+							onClick={e => {
+								onsignInClicked(email, password);
+								e.preventDefault();
+							}}>
+							Submit
+						</button>
+					</form>
 				</div>
 			</div>
-			<div className="main">
-				<div className="col-md-6 col-sm-12">
-					<div className="login-form">
-						<form>
-							<div className="form-group">
-								<label>Email</label>
-								<input
-									type="text"
-									className="form-control"
-									placeholder="Email"
-									onChange={e => setEmail(e.target.value)}
-								/>
-							</div>
-							<div className="form-group">
-								<label>Password</label>
-								<input
-									type="password"
-									className="form-control"
-									placeholder="Password"
-									onChange={e => setPassword(e.target.value)}
-								/>
-							</div>
-							<button
-								type="submit"
-								className="btn btn-black"
-								onClick={e => {
-									onsignInClicked(email, password);
-									e.preventDefault();
-								}}>
-								Login
-							</button>
-						</form>
-					</div>
-				</div>
-			</div>
-		</span>
+		</div>
 	);
 };
+
+// <span>
+// 	<div className="sidenav">
+// 		<div className="login-main-text">
+// 			<h2>
+// 				Application
+// 				<br /> Login Page
+// 			</h2>
+// 			<p>Login or register from here to access.</p>
+// 		</div>
+// 	</div>
+// 	<div className="main">
+// 		<div className="col-md-6 col-sm-12">
+// 			<div className="login-form">
+// 				<form>
+// 					<div className="form-group">
+// 						<label>Email</label>
+// 						<input
+// 							type="text"
+// 							className="form-control"
+// 							placeholder="Email"
+// 							onChange={e => setEmail(e.target.value)}
+// 						/>
+// 					</div>
+// 					<div className="form-group">
+// 						<label>Password</label>
+// 						<input
+// 							type="password"
+// 							className="form-control"
+// 							placeholder="Password"
+// 							onChange={e => setPassword(e.target.value)}
+// 						/>
+// 					</div>
+// 					<button
+// 						type="submit"
+// 						className="btn btn-black"
+// 						onClick={e => {
+// 							onsignInClicked(email, password);
+// 							e.preventDefault();
+// 						}}>
+// 						Login
+// 					</button>
+// 				</form>
+// 			</div>
+// 		</div>
+// 	</div>
+// </span>
