9ff37cd61179aebd10cf113e052f4f57e0e0fec0
kevincastro015 <kevin.castro015@mymdc.net>
2021-08-10 13:36:39 +0000

    extraSpaceRemoved

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,31 +6,16 @@ import PeopleWorship from "../../img/PeopleWorshippingSmall.jpg";
 export const Home = () => {
 	return (
 		<div className="container">
-			<div className="card">
-				<div className="row text-over-image">
-					<div className="col-6 image">
-						<img src={PeopleWorship} />
-					</div>
-
-					<div className="col-6 text text-center text-white h1 h-100 align-self-center">
-						Our mission is
-						<span className="highlight-text"> simple,</span> to bring those who are far from God close to
-						him.
-					</div>
+			<div className="row text-over-image">
+				<div className="col-6 image">
+					<img src={PeopleWorship} />
 				</div>
-			</div>
 
-			{/* <div id="missionBackground" className="bg-dark text-light">
-				<img
-					src="https://i.ibb.co/t34tCR9/People-Worshipping-Small.jpg"
-					alt="People-Worshipping-Small"
-					border="0"
-				/>
-				<p className="carousel-caption">
+				<div className="col-6 text text-center text-white h1 h-100 align-self-center">
 					Our mission is
 					<span className="highlight-text"> simple,</span> to bring those who are far from God close to him.
-				</p>
-			</div> */}
+				</div>
+			</div>
 		</div>
 	);
 };
