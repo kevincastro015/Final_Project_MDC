223d6494526f47a24021974b5a82d61cf65281b4
Marcelo C Alves <marcelocalves@live.com>
2021-08-13 23:19:24 +0000

    getVerse on flux

--- src/js/component/Bible.js
+++ src/js/component/Bible.js
@@ -2,42 +2,9 @@ import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 
 export const Bible = () => {
+	const { store, actions } = useContext(Context);
 	const [verseRef, setVerseRef] = useState("");
 	const [verse, setVerse] = useState("");
-	const API_KEY = "f0c3f22e7fe62e154590fb25c584f048"; // Fill in with your own key.
-	// const verse = document.querySelector(`#verse-content`);
-	// const verseRef = document.querySelector(`#verse`);
-
-	const BIBLE_ID = `06125adad2d5898a-01`;
-
-	const VERSES = [
-		`MAT.5.16`,
-		`PSA.23`,
-		`1COR.4.4-8`,
-		`PHP.4.13`,
-		`JHN.3.16`,
-		`ROM.8.28`,
-		`ISA.41.10`,
-		`PSA.46.1`,
-		`GAL.5.22-23`,
-		`HEB.11.1`,
-		`2TI.1.7`,
-		`1COR.10.13`,
-		`PRO.22.6`,
-		`ISA.40.31`,
-		`JOS.1.9`,
-		`HEB.12.2`,
-		`MAT.11.27`,
-		`ROM.10.9-10`,
-		`PHP.2.3-4`,
-		`MAT.5.43-44`,
-		`JHN.14.6`,
-		`MAT.6.33`,
-		`MAT.22.37-40`,
-		`MRK.10.44-45`,
-		`JHN.4.13-14`,
-		`LUK.5.31-32`
-	];
 	useEffect(() => {
 		/**
 		 * EDIT THIS!
@@ -47,7 +14,7 @@ export const Bible = () => {
 		 * state.loadSomeData(); <---- calling this function from the flux.js actions
 		 *
 		 **/
-		getResults(verseID).then(data => {
+		actions.getVerse(verseID).then(data => {
 			const passage = data.passages[0];
 			let aux;
 			let script = "";
@@ -70,33 +37,9 @@ export const Bible = () => {
 			// verse.innerHTML = <div className="text eb-container">${passage.content}</div>;
 		});
 	}, []);
-	const verseIndex = Math.floor(Math.random() * VERSES.length);
-	const verseID = VERSES[verseIndex];
+	const verseIndex = Math.floor(Math.random() * store.verses.length);
+	const verseID = store.verses[verseIndex];
 
-	//const verseIndex = new Date().getDate();
-
-	function getResults(verseID) {
-		return new Promise((resolve, reject) => {
-			const xhr = new XMLHttpRequest();
-			xhr.withCredentials = false;
-
-			xhr.addEventListener(`readystatechange`, function() {
-				if (this.readyState === this.DONE) {
-					const { data, meta } = JSON.parse(this.responseText);
-
-					// _BAPI.t(meta.fumsId);
-					resolve(data);
-				}
-			});
-
-			xhr.open(`GET`, `https://api.scripture.api.bible/v1/bibles/${BIBLE_ID}/search?query=${verseID}`);
-			xhr.setRequestHeader(`api-key`, API_KEY);
-
-			xhr.onerror = () => reject(xhr.statusText);
-
-			xhr.send();
-		});
-	}
 	return (
 		<div className="container">
 			<div className="row ">
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,7 +2,35 @@ const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
-			user: ""
+			user: "",
+			verses: [
+				`MAT.5.16`,
+				`PSA.23`,
+				`1COR.4.4-8`,
+				`PHP.4.13`,
+				`JHN.3.16`,
+				`ROM.8.28`,
+				`ISA.41.10`,
+				`PSA.46.1`,
+				`GAL.5.22-23`,
+				`HEB.11.1`,
+				`2TI.1.7`,
+				`1COR.10.13`,
+				`PRO.22.6`,
+				`ISA.40.31`,
+				`JOS.1.9`,
+				`HEB.12.2`,
+				`MAT.11.27`,
+				`ROM.10.9-10`,
+				`PHP.2.3-4`,
+				`MAT.5.43-44`,
+				`JHN.14.6`,
+				`MAT.6.33`,
+				`MAT.22.37-40`,
+				`MRK.10.44-45`,
+				`JHN.4.13-14`,
+				`LUK.5.31-32`
+			]
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
@@ -12,6 +40,28 @@ const getState = ({ getStore, setStore }) => {
 			},
 			userLoggedOut: () => {
 				setStore({ user: "" });
+			},
+			getVerse: verseID => {
+				const API_KEY = "f0c3f22e7fe62e154590fb25c584f048";
+				const BIBLE_ID = `06125adad2d5898a-01`;
+				return new Promise((resolve, reject) => {
+					const xhr = new XMLHttpRequest();
+					xhr.withCredentials = false;
+
+					xhr.addEventListener(`readystatechange`, function() {
+						if (this.readyState === this.DONE) {
+							const { data } = JSON.parse(this.responseText);
+							resolve(data);
+						}
+					});
+
+					xhr.open(`GET`, `https://api.scripture.api.bible/v1/bibles/${BIBLE_ID}/search?query=${verseID}`);
+					xhr.setRequestHeader(`api-key`, API_KEY);
+
+					xhr.onerror = () => reject(xhr.statusText);
+
+					xhr.send();
+				});
 			}
 		}
 	};
